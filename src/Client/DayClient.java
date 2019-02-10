08aba1ea83158a5f7d09669afd59065e7da6fc2e
sopdu <admin@sopdu.ru>
2019-02-11 00:35:06 +0300

    Client

--- src/Client/DayClient.java
+++ src/Client/DayClient.java
@@ -2,7 +2,10 @@ package Client;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -10,10 +13,12 @@ public class DayClient {
 
     private static JTextArea textarea;
     private static JTextField textfields;
-    private static BufferedReader reader;
-    private static PrintWriter writer;
+    private static BufferedReader reader;  // получение сообщение
+    private static PrintWriter writer;       // отправка сообщений
+    private static String login;
 
     public static void main(String[] args){
+        login = JOptionPane.showInputDialog("введите ваше имя");
         go();
     }
 
@@ -33,14 +38,29 @@ public class DayClient {
         JButton buttonsend = new JButton("Отправить");
         JButton buttonreset = new JButton("Обновить");
 
+        // отправить
+        buttonsend.addActionListener(new listen());
+
+
+        // обновление
+        buttonreset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                writer.print("");
+            }
+        });
+
         panel.add(scrollpane);
         panel.add(textfields);
         panel.add(buttonsend);
+        /*
         try {
             setNet();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        */
+        setNet();
 
         frame.getContentPane().add(BorderLayout.CENTER, panel);
         frame.getContentPane().add(BorderLayout.NORTH, buttonreset);
@@ -49,7 +69,23 @@ public class DayClient {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    private static class listen implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+    /*
     private static void setNet() throws Exception {
         Socket socket = new Socket("127.0.0.1", 500);
+    }
+    */
+    private static void setNet(){
+        try{
+            Socket socket = new Socket("127.0.0.1", 5000);
+            InputStreamReader is = new InputStreamReader(socket.getInputStream());
+            reader = new BufferedReader(is);
+            writer = new PrintWriter(socket.getOutputStream());
+        } catch (Exception ex){}
     }
 }
