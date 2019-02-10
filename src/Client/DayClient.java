a043b01d93a76a2050c74410d6e52ef5ea48d146
sopdu <admin@sopdu.ru>
2019-02-11 00:12:08 +0300

    Client

--- src/Client/DayClient.java
+++ src/Client/DayClient.java
@@ -1,9 +1,17 @@
 package Client;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class DayClient {
 
+    private static JTextArea textarea;
+    private static JTextField textfields;
+    private static BufferedReader reader;
+    private static PrintWriter writer;
 
     public static void main(String[] args){
         go();
@@ -15,14 +23,33 @@ public class DayClient {
         frame.setLocationRelativeTo(null); // окно размещаем по центру экрана
         JPanel panel = new JPanel();
         JTextArea textarea = new JTextArea(15, 30);
-        textarea.setLineWrap(true);
-        textarea.setWrapStyleWord(true);
-        textarea.setEditable(false);
+        textarea.setLineWrap(true);  // перенос строки
+        //textarea.setWrapStyleWord(true);
+        textarea.setEditable(false);  // нельзя изменить пользователем
         JScrollPane scrollpane = new JScrollPane(textarea);
         scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         JTextField textfields = new JTextField(20);
         JButton buttonsend = new JButton("Отправить");
         JButton buttonreset = new JButton("Обновить");
+
+        panel.add(scrollpane);
+        panel.add(textfields);
+        panel.add(buttonsend);
+        try {
+            setNet();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, buttonreset);
+        frame.setSize(400, 350);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private static void setNet() throws Exception {
+        Socket socket = new Socket("127.0.0.1", 500);
     }
 }
